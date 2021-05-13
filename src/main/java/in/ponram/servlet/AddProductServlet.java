018d04e9b183d0488538a4789e05dc81761b20e8
Ponram R <ponr2637@global.chainsys.com>
2021-05-13 12:44:24 +0530

    fixing sonarcloud issues

--- src/main/java/in/ponram/servlet/AddProductServlet.java
+++ src/main/java/in/ponram/servlet/AddProductServlet.java
@@ -21,41 +21,42 @@ public class AddProductServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+			throws ServletException, IOException, NumberFormatException {
 		String itemId = request.getParameter("itemId");
 		String brandName = request.getParameter("brandName");
 		String itemName = request.getParameter("itemName");
 		String itemCategory = request.getParameter("itemCategory");
-		
+
 		int prize = 0;
 		try {
-			
+
 			prize = Integer.parseInt(request.getParameter("rate"));
 		} catch (NumberFormatException e) {
-			
-			String errorMessage = "Invalid prize";
-			response.sendRedirect("add_product.jsp?errorMessage=" +errorMessage);
+
+			String errorMessage = "Invalid rate";
+			response.sendRedirect("add_product.jsp?rate=" + errorMessage);
 		}
-		
+
 		int quantity = 0;
 		try {
-			
+
 			quantity = Integer.parseInt(request.getParameter("quantity"));
 		} catch (NumberFormatException e) {
-			
+
 			String errorMessage = "Invalid quantity";
-			response.sendRedirect("add_product.jsp?errorMessage=" + errorMessage);
+			response.sendRedirect("add_product.jsp?Quantity=" + errorMessage);
 		}
 		Product product1 = new Product(itemId, brandName, itemName, itemCategory, prize, quantity);
 
 		boolean success = ProductManager.addStock(product1);
 		if (success) {
-			
+
 			String infoMessage = "Product Added Successfully";
 			response.sendRedirect("add_product.jsp?infoMessage=" + infoMessage);
 		} else {
-			
+
 			String errorMessage = "Invalid product";
 			response.sendRedirect("add_product.jsp?errorMessage=" + errorMessage);
 		}
